1af7bc81092fb7b6aa19ecf2b4cc73627e99ee8d
thbourlove <thbourlove@gmail.com>
2014-04-11 14:23:54 +0800

    add tests for silex service

---
+++ tests/Provider/Silex/FeaturesServiceProviderTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Silex\Tests\Provider;
+
+use Silex\Application;
+use Feature\Provider\Silex\FeaturesServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
+
+class FeaturesServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRegister()
+    {
+        $app = new Application();
+
+        $app->register(new FeaturesServiceProvider);
+
+        $this->assertInstanceOf('Feature\Features', $app['features']);
+    }
+
+    public function testConfig()
+    {
+        $app = new Application();
+
+        $app->register(new FeaturesServiceProvider);
+
+        $app['features.config'] = array(
+            'featureA' => true,
+        );
+
+        $this->assertEquals(true, $app['features']->variant('featureA'));
+    }
+
+    public function testConfigAfterLoad()
+    {
+        $app = new Application();
+
+        $app->register(new FeaturesServiceProvider);
+
+        $this->assertEquals(false, $app['features']->variant('featureA'));
+
+        $app['features.config'] = array(
+            'featureA' => true,
+        );
+
+        $this->assertEquals(false, $app['features']->variant('featureA'));
+    }
+}
